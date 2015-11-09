01299f780c0b93d292924c8b1b7771dc60cfc71d
SimonMLGill <simondongill@gmail.com>
2015-11-09 12:11:23 +0100

    git push snake master

---
+++ src/ClientMain.java
@@ -0,0 +1,12 @@
+
+import logic.Logic;
+
+public class ClientMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Logic logic = new Logic();
+		logic.run();
+	}
+
+}
